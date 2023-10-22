f086f4ef025e34ea1f995ed52e1f45672a8e66ba
Jonathan Boiser Cañedo <112171542+arcyasou@users.noreply.github.com>
2023-10-22 12:33:11 +0800

    Farm.java

---
+++ Farm.java
@@ -0,0 +1,12 @@
+class Farm {
+    private String farmName;
+
+    public Farm(String farmName) {
+        this.farmName = farmName;
+    }
+
+    public void displayFarmInfo() {
+        System.out.println("Farm Name: " + farmName);
+    }
+}
+
